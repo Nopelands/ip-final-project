99a89fafb27d29740a99297984d2c79e4a44d3b2
Nopelands <rhas@cin.ufpe.br>
2019-05-27 12:09:52 -0300

    Update Mensageiro.java

--- src/mensageiro/Mensageiro.java
+++ src/mensageiro/Mensageiro.java
@@ -2,13 +2,13 @@ package mensageiro;
 import perfis.*;
 import mensagens.*;
 import grupos.*;
-//TODO import conversas.*;
+import conversas.*;
 
 public class Mensageiro {
     private CadastroPerfis cadastroPerfis;
     private CadastroMensagens cadastroMensagens;
     //TODO private CadastroGrupos cadastroGrupos;
-    //TODO private CadastroStatus cadastroStatus;
+    //TODO private CadastroConversas cadastroConversas;
 
     //TODO cadastrar()
     //TODO criarGrupo()
