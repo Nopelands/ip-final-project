64cfbc5b194fa668eeefdd5a12a6656dc7404d7e
matheusepitacio <47263211+matheusepitacio@users.noreply.github.com>
2019-05-27 09:50:37 -0300

    Add files via upload

--- src/mensagem/Mensagem.java
+++ src/mensagem/Mensagem.java
@@ -1,20 +1,22 @@
 package mensagens;
 
 public abstract class Mensagem {
-    String mensagem;
-    MensagensRepositorio mensagens;
+    protected String mensagem;
+    protected String remetente;
 
-    public Mensagem(String mensagem, MensagensRepositorio mensagensRepositorio ) {
+    public Mensagem(String remetente, String mensagem) {
+        this.remetente = remetente;
         this.mensagem = mensagem;
-        this.mensagens = mensagensRepositorio;
     }
-    public abstract void enviar(String message);
+    public abstract  void novamensagem(String mensagem);
 
     public String getMensagem() {
         return this.mensagem;
     }
-    public void retirar(String mensagem, MensagensRepositorio mensagens){
-        mensagens.remover(mensagem);
+    public String getRemetente() {
+        return remetente;
     }
-
+    public void setMensagem(String mensagemsetar) {
+        this.mensagem = mensagemsetar;
+    }
 }
--- src/mensagem/MensagemNaoEncontradaException.java
+++ src/mensagem/MensagemNaoEncontradaException.java
@@ -1,7 +1,7 @@
 package mensagens;
 
-public class MensagemNaoEncontradaException extends Exception{
+public class MensagemNaoEncontradaException extends Exception {
     MensagemNaoEncontradaException() {
-        super("Mensagem nao encontrada");
+        super("Mensagem não encontrada");
     }
 }
