4076e02d727b2c1f1412b9b7fa3237011ec1bf14
Nopelands <rhas@cin.ufpe.br>
2019-05-27 12:06:20 -0300

    Create Mensageiro.java

---
+++ src/mensageiro/Mensageiro.java
@@ -0,0 +1,15 @@
+package mensageiro;
+import perfis.*;
+import mensagens.*;
+//TODO import status.*;
+
+public class Mensageiro {
+    private CadastroPerfis cadastroPerfis;
+    private CadastroMensagens cadastroMensagens;
+    //TODO private CadastroGrupos cadastroGrupos;
+    //TODO private CadastroStatus cadastroStatus;
+
+    //TODO cadastrar()
+    //TODO criarGrupo()
+    //TODO more TODOs
+}
