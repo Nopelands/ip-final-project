4e568bb2e8ff3b9c6beb20a97f65b7f4c5a39906
Nopelands <rhas@cin.ufpe.br>
2019-05-27 09:30:38 -0300

    Update RepositorioPerfisLista.java

--- src/perfil/RepositorioPerfisLista.java
+++ src/perfil/RepositorioPerfisLista.java
@@ -52,4 +52,36 @@ public class RepositorioPerfisLista implements RepositorioPerfis{
             }
         }
     }
+    public Perfis procurar(String number) throws PerfilNotFoundException {
+        if (this.perfil == null) {
+            throw new PerfilNotFoundException();
+        } else if (this.perfil.getNumber().equals(number)) {
+            return this.perfil;
+        } else {
+            return this.next.procurar(number);
+        }
+    }
+
+    public boolean existe(String number) {
+        if (this.perfil == null) {
+            return false;
+        } else if (this.perfil.getNumber().equals(number)) {
+            return true;
+        } else {
+            return this.next.existe(number);
+        }
+    }
+
+    public void atualizar(Perfis perfil) throws PerfilNotFoundException{
+        if (this.existe(perfil.getNumber())) {
+            if (this.perfil.equals(perfil)) {
+                this.perfil = perfil;
+            } else {
+                this.atualizar(perfil);
+            }
+        } else {
+            throw new PerfilNotFoundException();
+        }
+    }
+
 }
