44fd5846997c90c955c8db8d0ebb194ca53f580c
Gustavo F. Farias <gff@cin.ufpe.br>
2019-05-27 09:52:49 -0300

    adicionado o pacote conversas

---
+++ src/conversas/Conversa.java
@@ -0,0 +1,11 @@
+package conversas;
+
+import mensagens.*;
+import perfis.*;
+
+
+public class Conversa {
+	private MensagensRepositorio mensagens;
+	private Perfil emissor;
+	private Perfil receptor;
+}
