ab133cdf3f5b175cece3f4e9eec38e7425e4a78d
MatheusDalia <48736559+MatheusDalia@users.noreply.github.com>
2019-05-29 12:07:46 -0300

    Update Mensageiro.java

--- src/mensageiro/Mensageiro.java
+++ src/mensageiro/Mensageiro.java
@@ -9,6 +9,37 @@ public class Mensageiro {
     private CadastroMensagens cadastroMensagens;
     //TODO private CadastroGrupos cadastroGrupos;
     //TODO private CadastroConversas cadastroConversas;
+    private CadastroGrupos grupos;
+    
+    public Mensageiro(RepositorioGrupos repositorioGrupos) {
+    	this.grupos = new CadastroGrupos(repositorioGrupos);
+    }
+    
+    //NegocioGrupos
+    
+    public void inserir (Grupos grupo) throws GrupoJaCadastradoException {
+		grupos.inserir(grupo);
+    }
+    
+    public void remover (String nome) throws GrupoNaoEncontradoException{
+    	grupos.remover(nome);
+    }
+    
+    public Grupos procurar(String nome) throws GrupoNaoEncontradoException{
+    	return grupos.procurar(nome);
+    }
+    
+    public void atualizarGrupo(String nome, Grupos grupo) throws GrupoNaoEncontradoException, GrupoJaCadastradoException{
+    	grupos.atualizarGrupo(nome, grupo);
+    }
+    
+    public void atualizarDescricao(String descricao_Antigo, String descricao_Novo) throws GrupoNaoEncontradoException{
+    	grupos.atualizarDescricao(descricao_Antigo, descricao_Novo);
+    }
+    
+    public boolean checarGrupo (String nome) {
+    	return grupos.checarGrupo(nome);
+    }
 
     //TODO cadastrar()
     //TODO criarGrupo()
