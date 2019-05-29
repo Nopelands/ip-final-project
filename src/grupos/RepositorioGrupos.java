4d59e5b95a88cff71aca77c25d6a605680018b3f
MatheusDalia <48736559+MatheusDalia@users.noreply.github.com>
2019-05-29 14:56:49 -0300

    Update RepositorioGrupos.java

--- src/grupos/RepositorioGrupos.java
+++ src/grupos/RepositorioGrupos.java
@@ -4,13 +4,13 @@ public interface RepositorioGrupos {
 
 	public void inserir (Grupos grupo) throws GrupoJaCadastradoException;
 	
-	public void remover (String nome) throws GrupoNaoEncontradoException;
+	public void remover (Grupos grupo) throws GrupoNaoEncontradoException;
 	
 	public Grupos procurar(String nome) throws GrupoNaoEncontradoException;
 	
-	public void atualizarGrupo(String nome, Grupos grupo) throws GrupoNaoEncontradoException, GrupoJaCadastradoException;
+	public void atualizarNome(Grupos grupo, String nome_Novo) throws GrupoNaoEncontradoException;
 	
-	public void atualizarDescricao(String descricao_Antigo, String descricao_Novo) throws GrupoNaoEncontradoException;
+	public void atualizarDescricao(Grupos grupo, String descricao_Novo) throws GrupoNaoEncontradoException;
 	
 	public boolean checarGrupo (String nome);
 }
