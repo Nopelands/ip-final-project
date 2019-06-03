c751fd5b3b5d8ebbac26ff00831e1c39e652abdd
MatheusDalia <48736559+MatheusDalia@users.noreply.github.com>
2019-06-03 08:51:18 -0300

    Update CadastroGrupos.java

--- src/grupos/CadastroGrupos.java
+++ src/grupos/CadastroGrupos.java
@@ -27,24 +27,9 @@ public class CadastroGrupos implements RepositorioGrupos{
 		repositorio.remover(grupo);
 	}
 	
-	public void inserirPerfil(Grupos grupo, Perfil perfil) throws PerfilJaCadastradoNoGrupoException{
-		if (!grupo.getListaNomes().existe(perfil.getNumber())) {
-			grupo.getListaNomes().inserir(perfil);
-        } else {
-            throw  new PerfilJaCadastradoNoGrupoException();
-        }
-	}
-	
-	public void removerPerfil(Grupos grupo, Perfil perfil) throws PerfilNotFoundException {
-		grupo.getListaNomes().remover(perfil.getNumber());
-    }
-
-	public void atualizarDescricao(Grupos grupo, String descricao_Novo) throws GrupoNaoEncontradoException{
-		repositorio.atualizarDescricao(grupo, descricao_Novo);
+	public void atualizar(Grupos grupo) throws GrupoNaoEncontradoException{
+		repositorio.atualizar(grupo);
 	}
 
-	public void atualizarNome(Grupos grupo, String nome_Novo) throws GrupoNaoEncontradoException{
-		repositorio.atualizarNome(grupo, nome_Novo);
-	}
 
 }
