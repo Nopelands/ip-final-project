17ca96252a28d4a4c1bddbca940eec7d1f6c164d
MatheusDalia <48736559+MatheusDalia@users.noreply.github.com>
2019-06-05 13:35:14 -0300

    Update and rename RementeIntrusoException.java to RemetenteIntrusoException.java

--- src/grupos/RemetenteIntrusoException.java
+++ src/grupos/RemetenteIntrusoException.java
@@ -2,11 +2,11 @@ package grupos;
 
 import perfis.Perfil;
 
-public class RementeIntrusoException {
+public class RemetenteIntrusoException extends Exception {
 	private Grupo invadido;
 	private Perfil intruso;
 	public RemetenteIntrusoException (Grupo invadido, Perfil intruso) {
-		super ("O usuario " + intruso.getName() " nao esta no grupo " + invadido.getNome() + ", mas tentou enviar uma mensagem para ele");
+		super ("O usuario " + intruso.getName() + " nao esta no grupo " + invadido.getNome() + ", mas tentou enviar uma mensagem para ele");
 		this.invadido = invadido;
 		this.intruso = intruso;
 	}
