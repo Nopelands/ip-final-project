42ba069fe702af22268a1c4f18f6b7667711db1b
MatheusDalia <48736559+MatheusDalia@users.noreply.github.com>
2019-06-05 11:49:17 -0300

    Update GrupoTeste.java

--- src/grupos/GrupoTeste.java
+++ src/grupos/GrupoTeste.java
@@ -8,24 +8,24 @@ import conversas.*;
 public class GrupoTeste {
     public static void main(String[] args) throws GrupoJaCadastradoException, GrupoNaoEncontradoException {
         //Testando a classe Grupos
-    	Mensageiro mensageiro = new Mensageiro(new CadastroPerfis(new RepositorioPerfisArray()), new CadastroConversas(new RepositorioConversasArray(1000)), new CadastroGrupos(new RepositorioGruposArray()), new CadastroMensagens(new RepositorioMensagensArray()));
-		Grupos test1 = new Grupos("Familia", "Galera da farofa", new RepositorioPerfisLista(), new RepositorioMensagensArray());
-		Grupos test2 = new Grupos("Amigos", "Galera do pagode", new RepositorioPerfisLista(), new RepositorioMensagensArray());
-		Grupos test3 = new Grupos("Faculdade", "UFPE", new RepositorioPerfisLista(), new RepositorioMensagensArray());
-		Perfil perfil1 = new Perfil("Mainha", "9876-5324", new RepositorioPerfisLista());
+    	Mensageiro mensageiroArray = new Mensageiro(new CadastroPerfis(new RepositorioPerfisArray()), new CadastroConversas(new RepositorioConversasArray(1000)), new CadastroGrupos(new RepositorioGruposArray()), new CadastroMensagens(new RepositorioMensagensArray()));
+		Grupos test1 = new Grupos("Familia", "Galera da farofa", new RepositorioPerfisArray(), new RepositorioMensagensArray());
+		Grupos test2 = new Grupos("Amigos", "Galera do pagode", new RepositorioPerfisArray(), new RepositorioMensagensArray());
+		Grupos test3 = new Grupos("Faculdade", "UFPE", new RepositorioPerfisArray(), new RepositorioMensagensArray());
+		Perfil perfil1 = new Perfil("Mainha", "9876-5324", new RepositorioPerfisArray());
 		Mensagem mensagem1 = new MensagemCodificada(perfil1, "Traz o feijao", 001);
 		
 		//GRUPOS ARRAY
 		System.out.println("Teste da classe Grupos\nRepositorio em Array:");
 		    
 		    System.out.println("Inserindo grupos no repositório: ");		
-            mensageiro.inserir(test1);
-            mensageiro.inserir(test2);
-            mensageiro.inserir(test3);
+            mensageiroArray.inserir(test1);
+            mensageiroArray.inserir(test2);
+            mensageiroArray.inserir(test3);
             
-            if (mensageiro.checarGrupo(test1.getNome())
-    				&& mensageiro.checarGrupo(test2.getNome())
-    				&& mensageiro.checarGrupo(test3.getNome())) {
+            if (mensageiroArray.checarGrupo(test1.getNome())
+    				&& mensageiroArray.checarGrupo(test2.getNome())
+    				&& mensageiroArray.checarGrupo(test3.getNome())) {
     			System.out.println(
     					"O grupo " + test1.getNome() + " foi inserido.");
     			System.out.println("O grupo " +test2.getNome()
@@ -35,9 +35,9 @@ public class GrupoTeste {
     		}
             
             
-            mensageiro.remover(test2);
+            mensageiroArray.remover(test2);
             
-            if (!mensageiro.checarGrupo("Amigos")) {
+            if (!mensageiroArray.checarGrupo("Amigos")) {
     			System.out.println("O grupo "+ test2.getNome()  + " foi removido!\n");
     		} else {
     			System.out.println("O grupo nao foi removido!\n");
