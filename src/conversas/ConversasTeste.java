145b46f8031aa9f492457d6e664a6e00af9e835b
Gustavo F. de Farias <gff@cin.ufpe.br>
2019-06-07 10:03:32 -0300

    Mega comitão

--- src/conversas/ConversasTeste.java
+++ src/conversas/ConversasTeste.java
@@ -5,18 +5,24 @@ import mensagens.*;
 
 public class ConversasTeste {
 	public static void main (String[] args) {
+		CadastroConversas conversasArray = new CadastroConversas(new RepositorioConversasArray(2));
+		
 		CadastroPerfis perfis = new CadastroPerfis (new RepositorioPerfisArray());
 		Perfil usuario1 = new Perfil ("Sergio", "99140-2381", new RepositorioPerfisArray());
 		Perfil usuario2 = new Perfil ("Ricardo", "98865-0896", new RepositorioPerfisLista());
 		try {
 			perfis.cadastrar(usuario1);
+			System.out.println ("O usuario " + usuario1.getName() + " / " + usuario1.getNumber() + " foi cadastrado com sucesso.");
 			perfis.cadastrar(usuario2);
+			System.out.println ("O usuario " + usuario2.getName() + " / " + usuario2.getNumber() + " foi cadastrado com sucesso.");
 		} catch (PerfilJaCadastradoException e) {
 			System.out.println(e.getMessage());
 		}
 		try {
-			perfis.adicionarContato(usuario1.getName(), usuario2.getName());
-			perfis.adicionarContato(usuario2.getName(), usuario1.getName());
+			perfis.adicionarContato(usuario1.getNumber(), usuario2.getNumber());
+			System.out.println(usuario2.getName() + " foi adicionado à lista de contatos de " + usuario1.getName());
+			perfis.adicionarContato(usuario2.getNumber(), usuario1.getNumber());
+			System.out.println(usuario1.getName() + " foi adicionado à lista de contatos de " + usuario2.getName());
 		} catch (PerfilJaCadastradoException e1) {
 			System.out.println(e1.getMessage());
 		} catch (PerfilNotFoundException e2) {
@@ -28,10 +34,11 @@ public class ConversasTeste {
 		RepositorioMensagens rep = new RepositorioMensagensArray();
 		rep.inserir(m1);
 		
-		CadastroConversas conversasArray = new CadastroConversas(new RepositorioConversasArray(4));
+		Conversa conversa1 = new Conversa(usuario1, usuario2, rep);
 		
 		try {
-			conversasArray.iniciarConversa(new Conversa(usuario1, usuario2, rep));
+			conversasArray.iniciarConversa(conversa1);
+			System.out.println("A conversa entre " + conversa1.getEmissor().getName() + " e " + conversa1.getReceptor().getName() + " foi iniciada com sucesso.");
 		} catch (RepositorioException e1) {
 			System.out.println(e1.getMessage());
 		} catch (ConversaReiniciadaException e2) {
@@ -40,7 +47,34 @@ public class ConversasTeste {
 			System.out.println(e3.getMessage());
 		}
 		
-		CadastroConversas conversasLista = new CadastroConversas(new RepositorioConversasLista());
+		conversa1.inverter();
+		System.out.println("Emissor: " + conversa1.getEmissor().getNumber() + ", Receptor: " + conversa1.getReceptor().getNumber());
+		try {
+			conversasArray.iniciarConversa(conversa1);
+			System.out.println("A conversa entre " + conversa1.getEmissor().getName() + " e " + conversa1.getReceptor().getName() + " foi iniciada com sucesso.");
+		} catch (RepositorioException e1) {
+			System.out.println(e1.getMessage());
+		} catch (ConversaReiniciadaException e2) {
+			System.out.println(e2.getMessage());
+		} catch (NaoSaoContatosException e3) {
+			System.out.println(e3.getMessage());
+		}
 		
+		Mensagem m2 = new MensagemCodificada (usuario2, "Fala, Sergio!", ++contMensagens);
+		conversa1.inserir(m2);
+		
+		try {
+			conversasArray.iniciarConversa(conversa1);
+			System.out.println("A conversa entre " + conversa1.getEmissor().getName() + " e " + conversa1.getReceptor().getName() + " foi iniciada com sucesso.");
+		} catch (RepositorioException e1) {
+			System.out.println(e1.getMessage());
+		} catch (ConversaReiniciadaException e2) {
+			System.out.println(e2.getMessage());
+		} catch (NaoSaoContatosException e3) {
+			System.out.println(e3.getMessage());
+		}
+		
+		
+		// TODO CadastroConversas conversasLista = new CadastroConversas(new RepositorioConversasLista());
 	}
 }
