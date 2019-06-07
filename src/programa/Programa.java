06f167998d927927461f0de4b87fae3f9789b890
Nopelands <rhas@cin.ufpe.br>
2019-06-06 21:02:45 -0300

    Update programa.java to comply with sergio (tm) standards

--- src/programa/Programa.java
+++ src/programa/Programa.java
@@ -10,17 +10,18 @@ public class Programa {
     public static void main(String[] args) {
         Mensageiro mensageiro = new Mensageiro(new CadastroPerfis(new RepositorioPerfisArray()), new CadastroConversas(new RepositorioConversasArray(1000)), new CadastroGrupos(new RepositorioGruposArray()), new CadastroMensagens(new RepositorioMensagensArray()));
         try {
-            mensageiro.criarUser(true, "aaa", "12345");
-            mensageiro.criarUser(true, "bbb", "7272");
-            mensageiro.criarUser(true, "ccc", "62137");
-            mensageiro.criarUser(true, "ddd", "9999");
-            mensageiro.criarUser(true, "eee", "0001");
+            mensageiro.criarUser(new Perfil("aaa", "177013", new RepositorioPerfisArray()));
+            mensageiro.criarUser(new Perfil("bbb", "11037", new RepositorioPerfisArray()));
+            mensageiro.criarUser(new Perfil("ccc", "isso nem precisa ser um numero kk", new RepositorioPerfisArray()));
+            mensageiro.criarUser(new Perfil("ninguem vai entender as referencias mesmo", "1.048596", new RepositorioPerfisArray()));
+            mensageiro.criarUser(new Perfil("emma", "63194", new RepositorioPerfisArray()));
+            mensageiro.criarUser(new Perfil("god", "217448", new RepositorioConversasArray));
         } catch (PerfilJaCadastradoException e) {
             e.printStackTrace();
         }
         try {
-            mensageiro.adicionarContato("0001", "7272");
-            mensageiro.adicionarContato("0001", "9999");
+            mensageiro.adicionarContato("11037", "63194");
+            mensageiro.adicionarContato("11037", "217448");
         } catch (PerfilJaCadastradoException e) {
             e.printStackTrace();
         } catch (PerfilNotFoundException e) {
